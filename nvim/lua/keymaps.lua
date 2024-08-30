bf6978738216b8f90bfb824986537fff22b1056e
Nishan Bista <nishanbista525@gmail.com>
2024-08-30 23:27:12 +0545

    waybar updated

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -1,6 +1,7 @@
 vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left toggle<CR>")
 vim.keymap.set("n", "<leader>e", ":Neotree focus<CR>")
 vim.keymap.set("n", "<leader>ff", ":Telescope find_files<CR>")
-vim.keymap.set("n", "<leader>th", ":Telescope colorscheme<CR>") 
+vim.keymap.set("n","<leader>fs",":Telescope live_grep<CR>")
+vim.keymap.set("n", "<leader>th", ":Telescope colorscheme<CR>")
 vim.keymap.set("n", "<leader>ft", vim.lsp.buf.format, {})
 vim.keymap.set("n", "<m-tab>", ":BufferLineCycleNext<CR>")
