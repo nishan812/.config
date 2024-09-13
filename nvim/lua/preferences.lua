9220bb71ebeef4602225d15c858a8b7d21dc83dc
Nishan Bista <nishanbista525@gmail.com>
2024-09-13 21:42:55 +0545

    update

--- nvim/lua/preferences.lua
+++ nvim/lua/preferences.lua
@@ -1,3 +1,8 @@
 
 vim.cmd("set number")
 
+-- Set tab width to 4 spaces
+vim.opt.tabstop = 4        -- Number of spaces tabs count for
+vim.opt.shiftwidth = 2     -- Number of spaces to use for autoindent
+vim.opt.expandtab = true   -- Convert tabs to spaces
+
